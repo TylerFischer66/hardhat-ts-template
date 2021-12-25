6978daa22a2eb895e8f360ba58e906258dc0b4b5
methoddev123 <methoddev123@gmail.com>
2021-12-25 10:41:42 -0600

    Added deploy script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,7 +11,7 @@ dotenv.config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+task("deployKovan", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
@@ -24,20 +24,13 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 const config: HardhatUserConfig = {
   solidity: "0.6.6",
+  defaultNetwork: "kovan",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    kovan: {
+      url: "https://kovan.infura.io/v3/ba4a0bdfd515437bb4f90357fd5758fe",
+      accounts: [`e59531fb18930ee6d564d02c7f466725b21db879fe1c96d9767caeb6ae69c23c`],
     },
   },
-  // gasReporter: {
-  //   enabled: process.env.REPORT_GAS !== undefined,
-  //   currency: "USD",
-  // },
-  // etherscan: {
-  //   apiKey: process.env.ETHERSCAN_API_KEY,
-  // },
 };
 
 export default config;
